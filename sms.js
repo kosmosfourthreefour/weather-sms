e20e002c5255391291d1e651f3bd81dfa8cdc0e6
kosmos434 <kosmos434@protonmail.com>
2020-07-06 20:55:15 -0500

    add axios get, todo

--- sms.js
+++ sms.js
@@ -1,22 +1,28 @@
 const axios = require("axios");
+const dotenv = require("dotenv").config();
 const ZIP = 65802;
-import API_KEY from "hide.js";
-let API = `https://api.openweathermap.org/data/2.5/weather?zip=${ZIP}&appid=${API_KEY}`;
-let test = "https://jsonplaceholder.typicode.com/posts/1";
+
+let API = `https://api.openweathermap.org/data/2.5/weather?zip=${ZIP}&appid=${process.env.SUPER_SECRET_API_KEY}`;
 let dangerWeatherCodes = {
   212: "heavy thunderstorm",
   511: "freezing rain",
   781: "tornado",
 };
 
-// axios
-//   .get(API)
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     // console.log(error);
-//   });
-console.log(API_KEY);
+axios
+  .get(API)
+  .then(function (response) {
+    // handle success
+    console.log(response.data.weather[0].id);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+
+/**
+ * TODO
+ * if response.data.weather[0].id is in dangerWeatherCodes
+ * start the twilio sms-alert
+ * otherwise return, maybe with a guard clause
+ */
